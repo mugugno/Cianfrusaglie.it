709bdb60057bdf34bd63dbf38b13c5227cec27f1
raffa50 <reloader90@gmail.com>
2016-05-02 16:26:58 +0200

    Database iniziale con utente
    TODO Identity

---
+++ Cianfrusaglie/src/Cianfrusaglie/Models/CianfrusaglieDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.Entity;
+
+namespace Cianfrusaglie.Models {
+   public class CianfrusaglieDbContext : DbContext {
+      public DbSet<User> Users { get; set; }
+
+      protected override void OnModelCreating( ModelBuilder modelBuilder ) {
+         modelBuilder.Entity<User>().HasIndex( u => u.Email );
+         modelBuilder.Entity<User>().HasIndex( u => u.NickName );
+      }
+   }
+}
---
+++ Cianfrusaglie/src/Cianfrusaglie/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cianfrusaglie.Models
+{
+    public class User
+    {
+      public int Id { get; set; }
+      [Required]
+      public string Email { get; set; }
+      [Required, MinLength(3), MaxLength(13)]
+      public string NickName { get; set; }
+      public DateTime BirthDate { get; set; }
+      [Required]
+      public string City { get; set; } //TODO tabella con le città?!
+      public virtual ICollection<User> BlockedUsers { get; set; }
+    }
+}
