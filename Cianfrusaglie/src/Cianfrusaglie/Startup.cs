04608bb5313fa4b1a3d1d9d3391df4e669ba4479
raffa50 <reloader90@gmail.com>
2016-05-03 11:58:12 +0200

    Fixed startup

--- Cianfrusaglie/src/Cianfrusaglie/Startup.cs
+++ Cianfrusaglie/src/Cianfrusaglie/Startup.cs
@@ -37,15 +37,15 @@ namespace Cianfrusaglie {
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
       public void Configure( IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory ) {
-         //loggerFactory.AddConsole( Configuration.GetSection( "Logging" ) );
-         //loggerFactory.AddDebug();
+         loggerFactory.AddConsole( Configuration.GetSection( "Logging" ) );
+         loggerFactory.AddDebug();
 
          if( env.IsDevelopment() ) {
-            //app.UseBrowserLink();
-            //app.UseDeveloperExceptionPage();
-            //app.UseDatabaseErrorPage();
+            app.UseBrowserLink();
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
          } else {
-            //app.UseExceptionHandler( "/Home/Error" );
+            app.UseExceptionHandler( "/Home/Error" );
 
             // For more details on creating database during deployment see http://go.microsoft.com/fwlink/?LinkID=615859
             try {
@@ -58,9 +58,9 @@ namespace Cianfrusaglie {
 
          app.UseIISPlatformHandler( options => options.AuthenticationDescriptions.Clear() );
 
-         //app.UseStaticFiles();
+         app.UseStaticFiles();
 
-         app.UseIdentity();
+         //app.UseIdentity();
 
          // To configure external authentication please see http://go.microsoft.com/fwlink/?LinkID=532715
 
