2a5420d612a239e9f3311efb1463ce72e86c5ba4
raffa50 <reloader90@gmail.com>
2016-05-06 12:45:34 +0200

    trasformato import Categorie

--- Cianfrusaglie/src/Cianfrusaglie/Models/ApplicationDbInitializer.cs
+++ Cianfrusaglie/src/Cianfrusaglie/Models/ApplicationDbInitializer.cs
@@ -8,94 +8,162 @@ namespace Cianfrusaglie.Models {
             ctx.Categories.Add( c );
             ctx.SaveChanges();
 
-            ctx.Categories.AddRange(
-               new Category { OverCategory = c, Name = "Libri" },
-               new Category { OverCategory = c, Name = "Musica" },
-               new Category { OverCategory = c, Name = "Film" }
-            );
+            var lmfLibri = new Category() { OverCategory = c, Name = "Libri" };
+            ctx.Categories.Add( lmfLibri );
             ctx.SaveChanges();
+
+            var lmfMusica = new Category { OverCategory = c, Name = "Musica" };
+            ctx.Categories.Add( lmfMusica );
+            ctx.SaveChanges();
+
+            var lmfFilm = new Category {OverCategory = c, Name = "Film"};
+            ctx.Categories.Add( lmfFilm );
+            ctx.SaveChanges();
+
 
             c = new Category() { Name = "Videogiochi e Console" };
             ctx.Categories.Add( c );
             ctx.SaveChanges();
 
-            ctx.Categories.AddRange(
-               new Category { OverCategory = c, Name = "Videogiochi" },
-               new Category { OverCategory = c, Name = "Console" }
-            );
+            var vcVideogiochi = new Category {OverCategory = c, Name = "Videogiochi"};
+            ctx.Categories.Add( vcVideogiochi );
             ctx.SaveChanges();
+
+            var vcConsole = new Category {OverCategory = c, Name = "Console"};
+            ctx.Categories.Add( vcConsole );
+            ctx.SaveChanges();
+
 
             c = new Category() { Name = "Arredamento e accessori per la casa" };
             ctx.Categories.Add( c );
             ctx.SaveChanges();
 
-            ctx.Categories.AddRange(
-               new Category { OverCategory = c, Name = "Mobili" },
-               new Category { OverCategory = c, Name = "Elettrodomestici" },
-               new Category { OverCategory = c, Name = "Illuminazione" },
-               new Category { OverCategory = c, Name = "Cucina" },
-               new Category { OverCategory = c, Name = "Arredamento" },
-               new Category { OverCategory = c, Name = "Accessori per la casa" }
-            );
+            var aacMobili = new Category {OverCategory = c, Name = "Mobili"};
+            ctx.Categories.Add( aacMobili );
             ctx.SaveChanges();
+
+            var aacElettrodomestici = new Category {OverCategory = c, Name = "Elettrodomestici"};
+            ctx.Categories.Add( aacElettrodomestici );
+            ctx.SaveChanges();
+
+            var aacIlluminazione = new Category {OverCategory = c, Name = "Illuminazione"};
+            ctx.Categories.Add( aacIlluminazione );
+            ctx.SaveChanges();
+
+            var aacCucina = new Category {OverCategory = c, Name = "Cucina"};
+            ctx.Categories.Add( aacCucina );
+            ctx.SaveChanges();
+
+            var aacArredamento = new Category {OverCategory = c, Name = "Arredamento"};
+            ctx.Categories.Add( aacArredamento );
+            ctx.SaveChanges();
+
+            var aacAcplc = new Category {OverCategory = c, Name = "Accessori per la casa"};
+            ctx.Categories.Add( aacAcplc );
+            ctx.SaveChanges();
+
 
             c = new Category() { Name = "Oggettistica" };
             ctx.Categories.Add( c );
             ctx.SaveChanges();
 
-            ctx.Categories.AddRange(
-               new Category { OverCategory = c, Name = "Giocattoli" },
-               new Category { OverCategory = c, Name = "Modellismo e collezionismo" },
-               new Category { OverCategory = c, Name = "Cianfrusaglie" }
-            );
+
+            var oGiocattoli = new Category {OverCategory = c, Name = "Giocattoli"};
+            ctx.Categories.Add( oGiocattoli );
             ctx.SaveChanges();
+
+            var oModCol = new Category {OverCategory = c, Name = "Modellismo e collezionismo"};
+            ctx.Categories.Add( oModCol );
+            ctx.SaveChanges();
+
+            var oCianfrusaglie = new Category {OverCategory = c, Name = "Cianfrusaglie"};
+            ctx.Categories.Add( oCianfrusaglie );
+            ctx.SaveChanges();
+
+
 
             c = new Category() { Name = "Sport Tempo libero Fai da te" };
             ctx.Categories.Add( c );
             ctx.SaveChanges();
 
-            ctx.Categories.AddRange(
-               new Category { OverCategory = c, Name = "Sport" },
-               new Category { OverCategory = c, Name = "Tempo libero" },
-               new Category { OverCategory = c, Name = "Fai da te" }
-            );
+
+            var stlftSport = new Category {OverCategory = c, Name = "Sport"};
+            ctx.Categories.Add( stlftSport );
             ctx.SaveChanges();
+
+            var stlfTempoLibero = new Category {OverCategory = c, Name = "Tempo libero"};
+            ctx.Categories.Add( stlfTempoLibero );
+            ctx.SaveChanges();
+
+            var stlfFaiDaTe = new Category {OverCategory = c, Name = "Fai da te"};
+            ctx.Categories.Add( stlfFaiDaTe );
+            ctx.SaveChanges();
+
 
             c = new Category() { Name = "Abbigliamento" };
             ctx.Categories.Add( c );
             ctx.SaveChanges();
 
-            ctx.Categories.AddRange(
-               new Category { OverCategory = c, Name = "Abbigliamento" },
-               new Category { OverCategory = c, Name = "Accessori" },
-               new Category { OverCategory = c, Name = "Scarpe" }
-            );
+
+            var aAbbigliamento = new Category {OverCategory = c, Name = "Abbigliamento"};
+            ctx.Categories.Add( aAbbigliamento );
             ctx.SaveChanges();
+
+            var aAccessori = new Category {OverCategory = c, Name = "Accessori"};
+            ctx.Categories.Add( aAccessori );
+            ctx.SaveChanges();
+
+            var aScarpe = new Category() { OverCategory = c, Name = "Scarpe" };
+            ctx.Categories.Add( aScarpe );
+            ctx.SaveChanges();
+            
 
             c = new Category() { Name = "Auto e Moto" };
             ctx.Categories.Add( c );
             ctx.SaveChanges();
 
-            ctx.Categories.AddRange(
-               new Category { OverCategory = c, Name = "Auto e Moto" },
-               new Category { OverCategory = c, Name = "Accessori" }
-            );
+            var amAutoMoto = new Category {OverCategory = c, Name = "Auto e Moto"};
+            ctx.Categories.Add( amAutoMoto );
             ctx.SaveChanges();
+
+            var amAccessori = new Category {OverCategory = c, Name = "Accessori"};
+            ctx.Categories.Add( amAccessori );
+            ctx.SaveChanges();
+
+
 
             c = new Category() { Name = "Elettronica" };
             ctx.Categories.Add( c );
             ctx.SaveChanges();
 
-            ctx.Categories.AddRange(
-               new Category { OverCategory = c, Name = "Computer" },
-               new Category { OverCategory = c, Name = "Cellulari" },
-               new Category { OverCategory = c, Name = "Tv e schermi" },
-               new Category { OverCategory = c, Name = "Audio" },
-               new Category { OverCategory = c, Name = "Accessori" },
-               new Category { OverCategory = c, Name = "Periferiche" },
-               new Category { OverCategory = c, Name = "Foto e videocamera" }
-            );
+            var eComputer = new Category {OverCategory = c, Name = "Computer"};
+            ctx.Categories.Add( eComputer );
             ctx.SaveChanges();
+
+            var eCellulari = new Category {OverCategory = c, Name = "Cellulari"};
+            ctx.Categories.Add( eCellulari );
+            ctx.SaveChanges();
+
+            var eTvSchermi = new Category {OverCategory = c, Name = "Tv e schermi"};
+            ctx.Categories.Add( eTvSchermi );
+            ctx.SaveChanges();
+
+            var eAudio = new Category {OverCategory = c, Name = "Audio"};
+            ctx.Categories.Add( eAudio );
+            ctx.SaveChanges();
+
+            var eAccessori = new Category {OverCategory = c, Name = "Accessori"};
+            ctx.Categories.Add( eAccessori );
+            ctx.SaveChanges();
+
+            var ePeriferiche = new Category {OverCategory = c, Name = "Periferiche"};
+            ctx.Categories.Add( ePeriferiche );
+            ctx.SaveChanges();
+
+            var eFV = new Category {OverCategory = c, Name = "Foto e videocamera"};
+            ctx.Categories.Add( eFV );
+            ctx.SaveChanges();
+
 
             c = new Category() { Name = "MisteryBox" };
             ctx.Categories.Add( c );
@@ -105,22 +173,33 @@ namespace Cianfrusaglie.Models {
             ctx.Categories.Add( c );
             ctx.SaveChanges();
 
-            ctx.Categories.AddRange(
-               new Category { OverCategory = c, Name = "Abbigliamento" },
-               new Category { OverCategory = c, Name = "Mobilio" },
-               new Category { OverCategory = c, Name = "Accessori" }
-            );
+
+            var bAbbigliamento = new Category {OverCategory = c, Name = "Abbigliamento"};
+            ctx.Categories.Add( bAbbigliamento );
             ctx.SaveChanges();
+
+            var bMobilio = new Category {OverCategory = c, Name = "Mobilio"};
+            ctx.Categories.Add( bMobilio );
+            ctx.SaveChanges();
+
+            var bAccessori = new Category {OverCategory = c, Name = "Accessori"};
+            ctx.Categories.Add( bAccessori );
+            ctx.SaveChanges();
+
+
 
             c = new Category() { Name = "Animali" };
             ctx.Categories.Add( c );
             ctx.SaveChanges();
 
-            ctx.Categories.AddRange(
-               new Category { OverCategory = c, Name = "Animali" },
-               new Category { OverCategory = c, Name = "Accessori" }
-            );
+            var anAnimali = new Category {OverCategory = c, Name = "Animali"};
+            ctx.Categories.Add( anAnimali );
             ctx.SaveChanges();
+
+            var anAccessori = new Category {OverCategory = c, Name = "Accessori"};
+            ctx.Categories.Add( anAccessori );
+            ctx.SaveChanges();
+
          }
       }
    }
