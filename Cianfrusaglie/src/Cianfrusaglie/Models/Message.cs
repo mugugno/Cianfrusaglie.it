4ae099fa401f63c2a68bafa436de9db9a2b0765b
raffa50 <reloader90@gmail.com>
2016-05-06 16:09:00 +0200

    Popolamento e fix Utente

--- Cianfrusaglie/src/Cianfrusaglie/Models/Message.cs
+++ Cianfrusaglie/src/Cianfrusaglie/Models/Message.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Data.Entity.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Cianfrusaglie.Models {
    public class Message {
@@ -11,9 +13,13 @@ namespace Cianfrusaglie.Models {
       [DataType( DataType.DateTime )]
       public DateTime DateTime { get; set; }
 
+      [ForeignKey("Sender")]
+      public int SenderId { get; set; }
       [Required]
       public User Sender { get; set; }
 
+      [ForeignKey( "Receiver" )]
+      public int ReceiverId { get; set; }
       [Required]
       public User Receiver { get; set; }
    }
