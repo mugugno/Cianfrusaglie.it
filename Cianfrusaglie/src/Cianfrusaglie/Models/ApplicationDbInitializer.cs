203fc099fc4c5bd0f9297dd284261df1e0519891
raffa50 <reloader90@gmail.com>
2016-05-06 11:04:19 +0200

    Impostazione struttura SeedDatabase

---
+++ Cianfrusaglie/src/Cianfrusaglie/Models/ApplicationDbInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Cianfrusaglie.Models {
+   public static class ApplicationDbInitializer {
+      public static void EnsureSeedData( this ApplicationDbContext ctx ) {
+         if( !ctx.Categories.Any() ) {
+            var c = new Category() {Name = "Libri Musica Film"};
+            ctx.Categories.Add( c );
+         }
+
+         ctx.SaveChanges();
+      }
+   }
+}
