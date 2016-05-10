fdc9fd396473f778d4c1a2efcd2013480d9da4ce
Matteo Musso <Matteo Musso>
2016-05-10 22:43:19 +0200

    View model + controller  inserimento annuncio completi al 90%. commit locale

---
+++ Cianfrusaglie/src/Cianfrusaglie/ViewModels/Announce/CreateAnnounceViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Cianfrusaglie.Models;
+
+namespace Cianfrusaglie.ViewModels.Announce
+{
+    public class CreateAnnounceViewModel
+    {
+        public CreateAnnounceViewModel()
+        {
+            FormFieldDictionary = new Dictionary<int, string>();
+
+        }
+
+
+        [Required]
+        [StringLength(128, ErrorMessage = "Il titolo deve contenere almeno 3 caratteri", MinimumLength = 3)]
+        [Display(Name = "Titolo")]
+        public string Title { get; set; }
+
+        [Display(Name = "Descrizione")]
+        public string Description { get; set; }
+
+        [Display(Name = "Distanza")]
+        public int Range { get; set; }
+
+        public Dictionary<int, string> FormFieldDictionary { get; set; }
+
+    }
+}
+
