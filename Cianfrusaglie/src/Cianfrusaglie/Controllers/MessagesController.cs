96cf69f80f849553c00bd37e622b1c6313dd784e
raffa50 <reloader90@gmail.com>
2016-05-10 14:44:17 +0200

    - possibilità di vedere gli utenti con cui si hanno avuto conversazioni (non testato) - visualizzare singola conversazione con un utente (non testato)

--- Cianfrusaglie/src/Cianfrusaglie/Controllers/MessagesController.cs
+++ Cianfrusaglie/src/Cianfrusaglie/Controllers/MessagesController.cs
@@ -1,120 +1,96 @@
 using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Mvc.Rendering;
-using Microsoft.Data.Entity;
 using Cianfrusaglie.Models;
 
-namespace Cianfrusaglie.Controllers
-{
-    public class MessagesController : Controller
-    {
-        private ApplicationDbContext _context;
+namespace Cianfrusaglie.Controllers {
+   public class MessagesController : Controller {
+      private readonly ApplicationDbContext _context;
 
-        public MessagesController(ApplicationDbContext context)
-        {
-            _context = context;    
-        }
+      public MessagesController( ApplicationDbContext context ) { _context = context; }
 
-        // GET: Messages
-        public IActionResult Index(){
-            return View( _context.Messages.Where( u => u.Receiver.Id == User.GetUserId() ).ToList() );
-        }
+      //tutti gli utenti con cui l'utente loggato ha messaggiato
+      // GET: Messages
+      public IActionResult Index() { 
+         var usr = _context.Users.Single( u => u.Id == User.GetUserId() );
+         var userWitchIHaveMessaged = usr.SentMessages.Select( m => m.Receiver );
+         var userThatSendedMeMessage = usr.ReceivedMessages.Select( m => m.Sender );
 
-        // GET: Messages/Details/5
-        public IActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return HttpNotFound();
-            }
+         var users = userWitchIHaveMessaged.Union( userThatSendedMeMessage ).Distinct();
 
-            Message message = _context.Messages.Single(m => m.Id == id);
-            if (message == null)
-            {
-                return HttpNotFound();
-            }
+         return View( users.ToList() );
+      }
 
-            return View(message);
-        }
+      // GET: Messages/Details/5
+      public IActionResult Details( string id ) {
+         if( id == null )
+            return HttpNotFound();
 
-        // GET: Messages/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+         var messages =
+            _context.Messages.Where(
+               m =>
+                  ( m.Sender.Id == User.GetUserId() && m.Receiver.Id == id ) ||
+                  ( m.Receiver.Id == User.GetUserId() && m.Sender.Id == id ) );
 
-        // POST: Messages/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Message message)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Messages.Add(message);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(message);
-        }
+         return View( messages.ToList() );
+      }
 
-        // GET: Messages/Edit/5
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return HttpNotFound();
-            }
+      // GET: Messages/Create
+      public IActionResult Create() { return View(); }
 
-            Message message = _context.Messages.Single(m => m.Id == id);
-            if (message == null)
-            {
-                return HttpNotFound();
-            }
-            return View(message);
-        }
-
-        // POST: Messages/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Message message)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Update(message);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(message);
-        }
-
-        // GET: Messages/Delete/5
-        [ActionName("Delete")]
-        public IActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return HttpNotFound();
-            }
-
-            Message message = _context.Messages.Single(m => m.Id == id);
-            if (message == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(message);
-        }
-
-        // POST: Messages/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            Message message = _context.Messages.Single(m => m.Id == id);
-            _context.Messages.Remove(message);
+      // POST: Messages/Create
+      [HttpPost, ValidateAntiForgeryToken]
+      public IActionResult Create( Message message ) {
+         if( ModelState.IsValid ) {
+            _context.Messages.Add( message );
             _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-    }
+            return RedirectToAction( "Index" );
+         }
+         return View( message );
+      }
+
+      // GET: Messages/Edit/5
+      public IActionResult Edit( int? id ) {
+         if( id == null )
+            return HttpNotFound();
+
+         var message = _context.Messages.Single( m => m.Id == id );
+         if( message == null )
+            return HttpNotFound();
+         return View( message );
+      }
+
+      // POST: Messages/Edit/5
+      [HttpPost, ValidateAntiForgeryToken]
+      public IActionResult Edit( Message message ) {
+         if( ModelState.IsValid ) {
+            _context.Update( message );
+            _context.SaveChanges();
+            return RedirectToAction( "Index" );
+         }
+         return View( message );
+      }
+
+      // GET: Messages/Delete/5
+      [ActionName( "Delete" )]
+      public IActionResult Delete( int? id ) {
+         if( id == null )
+            return HttpNotFound();
+
+         var message = _context.Messages.Single( m => m.Id == id );
+         if( message == null )
+            return HttpNotFound();
+
+         return View( message );
+      }
+
+      // POST: Messages/Delete/5
+      [HttpPost, ActionName( "Delete" ), ValidateAntiForgeryToken]
+      public IActionResult DeleteConfirmed( int id ) {
+         var message = _context.Messages.Single( m => m.Id == id );
+         _context.Messages.Remove( message );
+         _context.SaveChanges();
+         return RedirectToAction( "Index" );
+      }
+   }
 }
