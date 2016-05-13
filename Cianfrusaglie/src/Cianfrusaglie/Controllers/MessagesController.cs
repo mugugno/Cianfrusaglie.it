ce9ea4d7654938a7118c04bd9195c03d59817bc5
Matteo Musso <Matteo Musso>
2016-05-14 00:00:40 +0200

    Rework quasi totale dell'interfaccia della messaggistica. 1) tutto in una sola schermata 2) schermata dinamica, interattiva (si colora la conversazione visualizzata) (distinzione per colori tra messaggi inviati e ricevuti) 3) contenuti con scrolldown, refresh intelligente dopo invio messaggio (ripristina la visualizzazione) TODO: chi poi riscrive la query, se riuscisse a ordinare la lista delle persone contattate per data e ora dell'ultimo messaggio scambiato con l'utente loggato, sarebbe fantastico!

--- Cianfrusaglie/src/Cianfrusaglie/Controllers/MessagesController.cs
+++ Cianfrusaglie/src/Cianfrusaglie/Controllers/MessagesController.cs
@@ -2,157 +2,150 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using Microsoft.AspNet.Mvc;
 using Cianfrusaglie.Models;
 using Cianfrusaglie.Statics;
 using Cianfrusaglie.ViewModels;
-using Microsoft.Data.Entity;
+using Microsoft.AspNet.Mvc;
 
 namespace Cianfrusaglie.Controllers {
-   public class MessagesController : Controller {
-      private readonly ApplicationDbContext _context;
+    public class MessagesController : Controller {
+        private readonly ApplicationDbContext _context;
 
-      public MessagesController( ApplicationDbContext context ) { _context = context; }
+        public MessagesController( ApplicationDbContext context ) { _context = context; }
 
-      public IEnumerable< Message > GetLoggedUsersMessagesWithUser( string id ) {
-         if( id == null )
-            throw new ArgumentNullException();
-
-         var messages =
-            _context.Messages.Where(
-               m =>
-                  ( m.Sender.Id == User.GetUserId() && m.Receiver.Id == id ) ||
-                  ( m.Receiver.Id == User.GetUserId() && m.Sender.Id == id ) );
-
-         return messages;
-      }
-
-        // FIX ABORTO DA RISCRIVERE
-        public string GetReceiver(Message message)
-        {
-            if (message == null)
+        public IEnumerable< Message > GetLoggedUsersMessagesWithUser( string id ) {
+            if( id == null )
                 throw new ArgumentNullException();
 
-            var receiver =
-               _context.Messages.Where( m => m.Equals( message )).Select( u => u.Receiver).Select( x => x.Id ).First();
+            IQueryable< Message > messages =
+                _context.Messages.Where(
+                    m =>
+                        ( m.Sender.Id == User.GetUserId() && m.Receiver.Id == id ) ||
+                        ( m.Receiver.Id == User.GetUserId() && m.Sender.Id == id ) );
+
+            return messages;
+        }
+
+        // FIX ABORTO DA RISCRIVERE
+        public string GetReceiver( Message message ) {
+            if( message == null )
+                throw new ArgumentNullException();
+
+            string receiver =
+                _context.Messages.Where( m => m.Equals( message ) ).Select( u => u.Receiver ).Select( x => x.Id ).First();
+
+            return receiver;
+        }
+
+        public User GetReceiverNew( Message message ) {
+            if( message == null )
+                throw new ArgumentNullException();
+
+            User receiver = _context.Messages.Where( m => m.Equals( message ) ).Select( u => u.Receiver ).First();
 
             return receiver;
         }
 
 
         protected IEnumerable< User > GetLoggedUsersConversationsUsers() {
-         var userThatSendedMeAMessage =
-            _context.Messages.Where( u => u.Sender.Id.Equals( User.GetUserId() ) ).Select( u => u.Receiver ).ToList();
-         var userThatISentAMessage =
-            _context.Messages.Where( u => u.Receiver.Id.Equals( User.GetUserId() ) ).Select( u => u.Sender ).ToList();
+            List< User > userThatSendedMeAMessage =
+                _context.Messages.Where( u => u.Sender.Id.Equals( User.GetUserId() ) ).Select( u => u.Receiver ).ToList();
+            List< User > userThatISentAMessage =
+                _context.Messages.Where( u => u.Receiver.Id.Equals( User.GetUserId() ) ).Select( u => u.Sender ).ToList();
 
 
-         userThatSendedMeAMessage.AddRange( userThatISentAMessage );
-         return userThatSendedMeAMessage.Distinct();
-      }
+            userThatSendedMeAMessage.AddRange( userThatISentAMessage );
+            return userThatSendedMeAMessage.Distinct();
+        }
 
-      //tutti gli utenti con cui l'utente loggato ha messaggiato
-      // GET: Messages
-      public IActionResult Index() {
-        ViewData["formCategories"] = _context.Categories.ToList();
-        ViewData["numberOfCategories"] = _context.Categories.ToList().Count;
-        if (!LoginChecker.HasLoggedUser(this))
-        return HttpBadRequest();
+        //tutti gli utenti con cui l'utente loggato ha messaggiato
+        // GET: Messages
+        public IActionResult Index( string id ) {
+            if( !LoginChecker.HasLoggedUser( this ) )
+                return HttpBadRequest();
+            List< User > users = GetLoggedUsersConversationsUsers().ToList();
+            var userAndMessagesDictionary = new Dictionary< User, Dictionary< Message, User > >();
+            foreach( User user in users ) {
+                var receiverDictionary = new Dictionary< Message, User >();
+                foreach( Message m in GetLoggedUsersMessagesWithUser( user.Id ).ToList() ) {
+                    receiverDictionary[ m ] = GetReceiverNew( m );
+                }
+                userAndMessagesDictionary[ user ] = receiverDictionary;
+            }
 
-         var users = GetLoggedUsersConversationsUsers().ToList();
-         return View( users );
-      }
+            ViewData[ "userAndMessagesDictionary" ] = userAndMessagesDictionary;
+            if( id != null )
+                ViewData[ "idAfterRefresh" ] = id;
+            else
+                ViewData[ "idAfterRefresh" ] = "";
+            return View();
+        }
 
-      // GET: Messages/Details/5
-      public IActionResult Details( string id ) {
+        // inviare un messaggio all'utente con id = id
+        // GET: Messages/Create
+        public IActionResult Create( string id ) {
+            if( !LoginChecker.HasLoggedUser( this ) )
+                return HttpBadRequest();
+            ViewData[ "formCategories" ] = _context.Categories.ToList();
+            ViewData[ "numberOfCategories" ] = _context.Categories.ToList().Count;
+            if( id == null )
+                return HttpNotFound();
 
-        ViewData["formCategories"] = _context.Categories.ToList();
-        ViewData["numberOfCategories"] = _context.Categories.ToList().Count;
-        if ( !LoginChecker.HasLoggedUser( this ) )
-        return HttpBadRequest();
+            if( !_context.Users.Any( u => u.Id == User.GetUserId() ) )
+                return HttpNotFound();
 
-         if( id == null )
-            return HttpNotFound();
+            ViewData[ "receiverId" ] = id;
+            return View();
+        }
 
-         var otherUser = _context.Users.SingleOrDefault( u => u.Id == id );
-         if( otherUser == null )
-            return HttpNotFound();
+        // POST: Messages/Create
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Create( MessageCreateViewModel messageCreate ) {
+            if( !LoginChecker.HasLoggedUser( this ) )
+                return HttpBadRequest();
 
-         ViewData[ "otherUser" ] = otherUser;
-         ViewData[ "messages" ] = GetLoggedUsersMessagesWithUser( id ).ToList();
-         ViewData[ "receiverId" ] = id;
-          var receiverList = new Dictionary<Message,string>();
-          foreach( Message m in GetLoggedUsersMessagesWithUser( id ).ToList() ) {
-              receiverList[ m ] = GetReceiver( m );
-          }
-          ViewData[ "receiverList" ] = receiverList;
+            if( messageCreate == null )
+                return HttpBadRequest();
 
+            if( ModelState.IsValid ) {
+                User loggedUsr = _context.Users.Single( u => u.Id == User.GetUserId() );
+                User receiverUsr = _context.Users.SingleOrDefault( u => u.Id == messageCreate.ReceiverId );
 
+                if( receiverUsr == null )
+                    return HttpBadRequest(); //id utente non valido
 
-         return View();
-      }
+                _context.Messages.Add( new Message {
+                    Sender = loggedUsr,
+                    Receiver = receiverUsr,
+                    Text = messageCreate.Text,
+                    DateTime = DateTime.Now
+                } );
+                _context.SaveChanges();
+                return RedirectToAction( "Index", new {id = receiverUsr.Id} );
+            }
+            return View( messageCreate );
+        }
 
-      // inviare un messaggio all'utente con id = id
-      // GET: Messages/Create
-      public IActionResult Create( string id ) {
+        // GET: Messages/Delete/5
+        [ActionName( "Delete" )]
+        public IActionResult Delete( int? id ) {
+            if( id == null )
+                return HttpNotFound();
 
-         if( !LoginChecker.HasLoggedUser( this ) )
-            return HttpBadRequest();
-        ViewData["formCategories"] = _context.Categories.ToList();
-        ViewData["numberOfCategories"] = _context.Categories.ToList().Count;
-        if ( id == null )
-            return HttpNotFound();
+            Message message = _context.Messages.SingleOrDefault( m => m.Id == id );
+            if( message == null )
+                return HttpNotFound();
 
-         if( !_context.Users.Any( u => u.Id == User.GetUserId() ) )
-            return HttpNotFound();
+            return View( message );
+        }
 
-         ViewData[ "receiverId" ] = id;
-         return View();
-      }
-
-      // POST: Messages/Create
-      [HttpPost, ValidateAntiForgeryToken]
-      public IActionResult Create( MessageCreateViewModel messageCreate ) {
-         if( !LoginChecker.HasLoggedUser( this ) )
-            return HttpBadRequest();
-
-         if( messageCreate == null )
-            return HttpBadRequest();
-
-         if( ModelState.IsValid ) {
-            var loggedUsr = _context.Users.Single( u => u.Id == User.GetUserId() );
-            var receiverUsr = _context.Users.SingleOrDefault( u => u.Id == messageCreate.ReceiverId );
-
-            if( receiverUsr == null )
-               return HttpBadRequest(); //id utente non valido
-
-            _context.Messages.Add( new Message() { Sender = loggedUsr, Receiver = receiverUsr, Text = messageCreate.Text, DateTime = DateTime.Now} );
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName( "Delete" ), ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed( int id ) {
+            Message message = _context.Messages.Single( m => m.Id == id );
+            _context.Messages.Remove( message );
             _context.SaveChanges();
-             return RedirectToAction( "Details", new {id = receiverUsr.Id} );
-         }
-         return View( messageCreate );
-      }
-
-      // GET: Messages/Delete/5
-      [ActionName( "Delete" )]
-      public IActionResult Delete( int? id ) {
-         if( id == null )
-            return HttpNotFound();
-
-         var message = _context.Messages.SingleOrDefault( m => m.Id == id );
-         if( message == null )
-            return HttpNotFound();
-
-         return View( message );
-      }
-
-      // POST: Messages/Delete/5
-      [HttpPost, ActionName( "Delete" ), ValidateAntiForgeryToken]
-      public IActionResult DeleteConfirmed( int id ) {
-         var message = _context.Messages.Single( m => m.Id == id );
-         _context.Messages.Remove( message );
-         _context.SaveChanges();
-         return RedirectToAction( "Index" );
-      }
-   }
+            return RedirectToAction( "Index" );
+        }
+    }
 }
