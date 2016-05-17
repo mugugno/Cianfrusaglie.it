58ca2c46112267848f257b36ca2384c3d6076fd3
raffa50 <reloader90@gmail.com>
2016-05-17 15:02:23 +0200

    fix dimenticanza

--- Cianfrusaglie/src/Cianfrusaglie/ViewModels/Announce/CreateAnnounceViewModel.cs
+++ Cianfrusaglie/src/Cianfrusaglie/ViewModels/Announce/CreateAnnounceViewModel.cs
@@ -28,9 +28,10 @@ namespace Cianfrusaglie.ViewModels.Announce {
 
         public Dictionary< int, string > FormFieldDictionary { get; set; }
 
-        public Dictionary< int, bool > CategoryDictionary { get; set; }
+      [EnsureMinimumElements( 1, ErrorMessage = "Almeno una categoria richiesta!" )]
+      public Dictionary< int, bool > CategoryDictionary { get; set; }
 
-         [EnsureMinimumElements( 1, ErrorMessage = "Almeno una categoria richiesta!" )]
+         [EnsureMinimumElements( 1, ErrorMessage = "Almeno una foto richiesta!" )]
          public ICollection< IFormFile > Photos { get; set; }
     }
 }
