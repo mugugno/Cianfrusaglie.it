3b08de929e4f9e8106ff6d4d31f4493b0f4126e4
raffa50 <reloader90@gmail.com>
2016-05-18 15:38:30 +0200

    fix js

--- Cianfrusaglie/src/Cianfrusaglie/wwwroot/js/googleMaps.js
+++ Cianfrusaglie/src/Cianfrusaglie/wwwroot/js/googleMaps.js
@@ -11,7 +11,12 @@ function unableToGeoLocalize(positionError) {
 
 var map, marker;
 
-function initializeGMaps(position) {
+/// inizializza Google Maps alla posizione position
+/// onlyView è true se non si può modificare la posizione del marker (usato con onlyView = true in visualizza annuncio o profilo utente)
+function initializeGMaps(position, onlyView) {
+    if( onlyView == null )
+        onlyView = false;
+
     var mapProp = {
         center: position,
         zoom: 5,
@@ -22,31 +27,31 @@ function initializeGMaps(position) {
 
     placeMarker(position);
 
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    if( !onlyView ) {
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[ google.maps.ControlPosition.TOP_LEFT ].push(input);
 
-    searchBox.addListener('places_changed', function () {
-        var places = searchBox.getPlaces();
+        searchBox.addListener('places_changed', function() {
+            var places = searchBox.getPlaces();
 
-        if (places.length == 0)
-            return;
+            if( places.length == 0 )
+                return;
 
-        var bounds = new google.maps.LatLngBounds();
+            var bounds = new google.maps.LatLngBounds();
 
-        if (places[0].geometry.viewport) {
-            bounds.union(places[0].geometry.viewport);
-        } else {
-            bounds.extend(places[0].geometry.location);
-        }
-        map.fitBounds(bounds);
+            if( places[ 0 ].geometry.viewport ) {
+                bounds.union(places[ 0 ].geometry.viewport);
+            } else {
+                bounds.extend(places[ 0 ].geometry.location);
+            }
+            map.fitBounds(bounds);
 
-        placeMarker(places[0].geometry.location, places[0].title);
-    });
+            placeMarker(places[ 0 ].geometry.location, places[ 0 ].title);
+        });
 
-    google.maps.event.addListener(map, 'click', function (event) {
-        placeMarker(event.latLng);
-    });
+        google.maps.event.addListener(map, 'click', function(event) { placeMarker(event.latLng); });
+    }
 }
 
 function placeMarker(location, title) {
