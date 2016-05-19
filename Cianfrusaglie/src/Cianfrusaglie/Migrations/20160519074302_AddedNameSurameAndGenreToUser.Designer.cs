8befebf0b204673b59510f869bea78680584dd29
Lorenzo Francone <Lorenzo Francone>
2016-05-19 09:51:23 +0200

    aggiunti nome, cognome e genere al db

--- Cianfrusaglie/src/Cianfrusaglie/Migrations/20160519074302_AddedNameSurameAndGenreToUser.Designer.cs
+++ Cianfrusaglie/src/Cianfrusaglie/Migrations/20160519074302_AddedNameSurameAndGenreToUser.Designer.cs
@@ -8,9 +8,10 @@ using Cianfrusaglie.Models;
 namespace Cianfrusaglie.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160519074302_AddedNameSurameAndGenreToUser")]
+    partial class AddedNameSurameAndGenreToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
