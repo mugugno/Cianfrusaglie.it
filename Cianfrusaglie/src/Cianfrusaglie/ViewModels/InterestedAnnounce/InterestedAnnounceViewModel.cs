0c317d5e517df5309637fbe4215b443aa0edf3f1
Luca Demetrio <luca.demetrio@outlook.it>
2016-05-19 17:25:05 +0200

    Aggiunto: 1) Controller per Interessati ad un mio annuncio; 2) View per la visualizzazione di tali utenti (da finire) 3) ViewModel per il passaggio dei dati.

---
+++ Cianfrusaglie/src/Cianfrusaglie/ViewModels/InterestedAnnounce/InterestedAnnounceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Cianfrusaglie.Models;
+
+namespace Cianfrusaglie.ViewModels.InterestedAnnounce
+{
+    public class InterestedAnnounceViewModel
+    {
+        [Required]
+        public Models.Announce  Announce { get; set; }
+
+        [Required]
+        public List<User> InterestedUsers { get; set; }
+    }
+}
---
+++ Cianfrusaglie/test/Cianfrusaglie.Tests/InterestedAnnounceControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cianfrusaglie.Controllers;
+using Microsoft.AspNet.Mvc;
+using Xunit;
+
+namespace Cianfrusaglie.Tests
+{
+    public class InterestedAnnounceControllerTests : BaseTestSetup
+    {
+        protected InterestedAnnounceController CreateInterestedAnnounceController( string id ) {
+            return new InterestedAnnounceController( Context ) {ActionContext = MockActionContextForLogin( id )};
+        }
+
+        [Fact]
+        public void UserVisualizeInterestedToAnnounceIsOk() {
+            string id = Context.Users.Single( u => u.UserName.Equals( FirstUserName ) ).Id;
+            var interestedController = CreateInterestedAnnounceController( id );
+            int announceId = Context.Announces.First( a => a.AuthorId.Equals( id ) ).Id;
+            var result = interestedController.Index( announceId );
+            Assert.IsType<ViewResult>( result );
+        }
+
+        [Fact]
+        public void UserTriesToViewInterestedOfAnnounceOfOtherUserAndFails() {
+            string id = Context.Users.Single(u => u.UserName.Equals(FirstUserName)).Id;
+            var interestedController = CreateInterestedAnnounceController(id);
+            int announceId = Context.Announces.First(a => a.Author.UserName.Equals(SecondUserName)).Id;
+            var result = interestedController.Index(announceId);
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public void UserTriesToViewInterestedToNonExistingAnnounceAndFails() {
+            string id = Context.Users.Single(u => u.UserName.Equals(FirstUserName)).Id;
+            var interestedController = CreateInterestedAnnounceController(id);
+            var result = interestedController.Index(909090);
+            Assert.IsType<HttpNotFoundResult>(result);
+        }
+
+        [Fact]
+        public void VisitorTriesToViewInterestedToAnnounceAndFails() {
+            var interestedController = CreateInterestedAnnounceController(null);
+            int announceId = Context.Announces.First(a => a.Author.UserName.Equals( FirstUserName )).Id;
+            var result = interestedController.Index(announceId);
+            Assert.IsType<BadRequestResult>(result);
+        }
+    }
+}
