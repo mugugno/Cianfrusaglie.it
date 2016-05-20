54aebcb3bbe13233a968a0ac965c1748602fe5f9
raffa50 <reloader90@gmail.com>
2016-05-20 15:56:59 +0200

    qualche annuncio in più

--- Cianfrusaglie/src/Cianfrusaglie/Models/BaseUserTestInitializer.cs
+++ Cianfrusaglie/src/Cianfrusaglie/Models/BaseUserTestInitializer.cs
@@ -14,7 +14,9 @@ namespace Cianfrusaglie.Models {
             BirthDate = new DateTime(1970,1,1),
             PhoneNumber = "123456789",
             Genre = 2,
-            PasswordHash = "cipolla!"
+            PasswordHash = "cipolla!",
+            Latitude = 44.40678,
+            Longitude = 8.93391
          };
 
          var user2 = new User() {
@@ -24,7 +26,9 @@ namespace Cianfrusaglie.Models {
             BirthDate = new DateTime( 1980, 7, 7 ),
             PhoneNumber = "123456789",
             Genre = 1,
-            PasswordHash = "cipolla!"
+            PasswordHash = "cipolla!",
+            Latitude = 44.40678,
+            Longitude = 8.93391
          };
 
          ctx.Users.AddRange( user1, user2 );
@@ -34,7 +38,10 @@ namespace Cianfrusaglie.Models {
             Author = user1,
             Title = "Telefonino Samsung con Android",
             Description = "Regalo telefonino Samsung con Android\ndisponibile lunedì pomeriggio 14-16",
-            PublishDate = DateTime.Now.AddDays(-3)
+            PublishDate = DateTime.Now.AddDays(-3),
+            Latitude = 44.40678,
+            Longitude = 8.93391,
+            MeterRange = 7
          };
 
          ctx.Announces.Add( announce1 );
@@ -58,7 +65,10 @@ namespace Cianfrusaglie.Models {
             Author = user2,
             Title = "Gattino appena nato di pochi mesi",
             Description = "Regalo gattino appena nato perchè non lo posso tenere.",
-            PublishDate = DateTime.Now.AddDays( -3 )
+            PublishDate = DateTime.Now.AddDays( -3 ),
+            Latitude = 44.40678,
+            Longitude = 8.93391,
+            MeterRange = 7
          };
 
          ctx.Announces.Add( announce2 );
@@ -69,12 +79,69 @@ namespace Cianfrusaglie.Models {
          ctx.SaveChanges();
 
          ctx.AnnounceFormFieldsValues.AddRange(
-            new AnnounceFormFieldsValues() { Announce = announce2, FormField = ctx.FormFields.Single( p => p.Name == "Marca" ), Value = "galaxy s5570" },
-            new AnnounceFormFieldsValues() { Announce = announce2, FormField = ctx.FormFields.Single( p => p.Name == "Stato" ), Value = "buono" }
+            new AnnounceFormFieldsValues() { Announce = announce2, FormField = ctx.FormFields.Single( p => p.Name == "Specie" ), Value = "gatto" },
+            new AnnounceFormFieldsValues() { Announce = announce2, FormField = ctx.FormFields.Single( p => p.Name == "Taglia Animale" ), Value = "mini" }
          );
          ctx.SaveChanges();
 
          ctx.ImageUrls.Add( new ImageUrl() { Announce = announce2, Url = @"/images/kitten.jpg" } );
+
+         ctx.SaveChanges();
+
+         var announce3 = new Announce() {
+            Author = user1,
+            Title = "Scarpe all star",
+            Description = "Regalo scarpe all star come nuove, mai usate",
+            PublishDate = DateTime.Now.AddDays( -3 ),
+            Latitude = 44.40678,
+            Longitude = 8.93391,
+            MeterRange = 7
+         };
+
+         ctx.Announces.Add( announce3 );
+         ctx.SaveChanges();
+
+         ctx.AnnounceCategories.Add(
+            new AnnounceCategory() { Announce = announce3, Category = ctx.Categories.Single( p => p.Name == "Scarpe" ) } );
+         ctx.SaveChanges();
+
+         ctx.AnnounceFormFieldsValues.AddRange(
+            new AnnounceFormFieldsValues() { Announce = announce3, FormField = ctx.FormFields.Single( p => p.Name == "Marca" ), Value = "all star" },
+            new AnnounceFormFieldsValues() { Announce = announce3, FormField = ctx.FormFields.Single( p => p.Name == "Colore" ), Value = "grigio" },
+            new AnnounceFormFieldsValues() { Announce = announce3, FormField = ctx.FormFields.Single( p => p.Name == "Taglia Scarpa" ), Value = "43" }
+         );
+         ctx.SaveChanges();
+
+         ctx.ImageUrls.Add( new ImageUrl() { Announce = announce3, Url = @"/images/all-star.jpg" } );
+
+         ctx.SaveChanges();
+
+
+         var announce4 = new Announce() {
+            Author = user2,
+            Title = "Tavolo da giardino con sedie",
+            Description = "Regalo tavolo da giardino con 8 sedie. Venite a prenderlo se lo volete",
+            PublishDate = DateTime.Now.AddDays( -3 ),
+            Latitude = 44.40678,
+            Longitude = 8.93391,
+            MeterRange = 0
+         };
+
+         ctx.Announces.Add( announce4 );
+         ctx.SaveChanges();
+
+         ctx.AnnounceCategories.Add(
+            new AnnounceCategory() { Announce = announce4, Category = ctx.Categories.Single( p => p.Name == "Mobili" ) } );
+         ctx.SaveChanges();
+
+         ctx.AnnounceFormFieldsValues.AddRange(
+            new AnnounceFormFieldsValues() { Announce = announce4, FormField = ctx.FormFields.Single( p => p.Name == "Stato" ), Value = "buono" },
+            new AnnounceFormFieldsValues() { Announce = announce4, FormField = ctx.FormFields.Single( p => p.Name == "Tipo Mobile" ), Value = "Tavolo" },
+            new AnnounceFormFieldsValues() { Announce = announce4, FormField = ctx.FormFields.Single( p => p.Name == "Materiale" ), Value = "plastica" }
+         );
+         ctx.SaveChanges();
+
+         ctx.ImageUrls.Add( new ImageUrl() { Announce = announce4, Url = @"/images/tavolo-da-giardino.jpg" } );
 
          ctx.SaveChanges();
       }
