34c277a297425691f8dab783d5cd7e073b9b79c2
raffa50 <reloader90@gmail.com>
2016-05-20 15:12:37 +0200

    fix creazione annuncio googlemaps (again)

--- Cianfrusaglie/src/Cianfrusaglie/Models/BaseUserTestInitializer.cs
+++ Cianfrusaglie/src/Cianfrusaglie/Models/BaseUserTestInitializer.cs
@@ -17,7 +17,15 @@ namespace Cianfrusaglie.Models {
             PasswordHash = "cipolla!"
          };
 
-         var user2 = new User() {};
+         var user2 = new User() {
+            Email = "giovanna.bianchi@email.it",
+            Name = "Giovanna",
+            UserName = "Giogio",
+            BirthDate = new DateTime( 1980, 7, 7 ),
+            PhoneNumber = "123456789",
+            Genre = 1,
+            PasswordHash = "cipolla!"
+         };
 
          ctx.Users.AddRange( user1, user2 );
          ctx.SaveChanges();
@@ -43,6 +51,30 @@ namespace Cianfrusaglie.Models {
          ctx.SaveChanges();
 
          ctx.ImageUrls.Add( new ImageUrl() { Announce = announce1, Url = @"/images/cell1.jpg"} );
+
+         ctx.SaveChanges();
+
+         var announce2 = new Announce() {
+            Author = user2,
+            Title = "Gattino appena nato di pochi mesi",
+            Description = "Regalo gattino appena nato perchè non lo posso tenere.",
+            PublishDate = DateTime.Now.AddDays( -3 )
+         };
+
+         ctx.Announces.Add( announce2 );
+         ctx.SaveChanges();
+
+         ctx.AnnounceCategories.Add(
+            new AnnounceCategory() { Announce = announce2, Category = ctx.Categories.Single( p => p.Name == "Animali" && p.OverCategory != null ) } );
+         ctx.SaveChanges();
+
+         ctx.AnnounceFormFieldsValues.AddRange(
+            new AnnounceFormFieldsValues() { Announce = announce2, FormField = ctx.FormFields.Single( p => p.Name == "Marca" ), Value = "galaxy s5570" },
+            new AnnounceFormFieldsValues() { Announce = announce2, FormField = ctx.FormFields.Single( p => p.Name == "Stato" ), Value = "buono" }
+         );
+         ctx.SaveChanges();
+
+         ctx.ImageUrls.Add( new ImageUrl() { Announce = announce2, Url = @"/images/kitten.jpg" } );
 
          ctx.SaveChanges();
       }
