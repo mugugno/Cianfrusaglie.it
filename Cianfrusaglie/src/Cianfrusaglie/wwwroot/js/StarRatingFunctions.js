f865e850edd4046464964353c6f5f8a171268d08
Luca Demetrio <uncd3m3@gmail.com>
2016-05-26 12:26:09 +0200

    Aggiunti file CSS, JS per lo StarRating. Aggiunto lo StarRating sulla pagina del Feedback, già collegato al campo asp-for. Funziona :3 Committo perché sennò perdo 1h e mezza di dolori.

---
+++ Cianfrusaglie/src/Cianfrusaglie/wwwroot/js/StarRatingFunctions.js
@@ -0,0 +1,6 @@
+﻿//Quando si clicca una stella, va ad impostare il Rating al valore corretto.
+function onStarClick(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement;
+    document.getElementById("AspStarRatingValue").value = target.value;
+}
