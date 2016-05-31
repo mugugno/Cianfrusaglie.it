5959458c89c3805bc59d0394f530d86c73c1bb61
raffa50 <reloader90@gmail.com>
2016-05-31 07:39:18 +0200

    refactoring home, messo include su autore

--- Cianfrusaglie/src/Cianfrusaglie/Controllers/HomeController.cs
+++ Cianfrusaglie/src/Cianfrusaglie/Controllers/HomeController.cs
@@ -1,62 +1,52 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using Cianfrusaglie.Constants;
 using Cianfrusaglie.Models;
 using Cianfrusaglie.Statics;
+using Cianfrusaglie.Suggestions;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Mvc;
-using static Cianfrusaglie.Constants.CommonFunctions;
-using Cianfrusaglie.Suggestions;
 using Microsoft.Data.Entity;
+using static Cianfrusaglie.Constants.CommonFunctions;
 
-namespace Cianfrusaglie.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly ApplicationDbContext _context;
-        public UserManager<User> UserManager;
-        public RankAlgorithm RankAlgorithm;
+namespace Cianfrusaglie.Controllers {
+   public class HomeController : Controller {
+      private readonly ApplicationDbContext _context;
+      public RankAlgorithm RankAlgorithm;
+      public UserManager< User > UserManager;
 
-        public HomeController(ApplicationDbContext context)
-        {
-            _context = context;
-            RankAlgorithm = new RankAlgorithm(context);
-        }
+      public HomeController( ApplicationDbContext context ) {
+         _context = context;
+         RankAlgorithm = new RankAlgorithm( context );
+      }
 
-        public IActionResult Index()
-        {
-            ViewData["listImages"] = _context.ImageUrls.ToList();
-            ViewData["listUsers"] = _context.Users.ToList();
-            //Include(u=>u.AnnounceCategories).
-            ViewData["listAnnounces"] = _context.Announces.OrderByDescending(u => u.PublishDate).Take(3).ToList();
+      public IActionResult Index() {
+         ViewData[ "listImages" ] = _context.ImageUrls.ToList();
+         //Include(u=>u.AnnounceCategories).
+         ViewData[ "listAnnounces" ] = _context.Announces.Include( a => a.Author ).OrderByDescending( u => u.PublishDate ).Take( 3 ).ToList();
 
-            if( LoginChecker.HasLoggedUser( this ) ) {
-                var user = _context.Users.Single( u => User.GetUserId().Equals( u.Id ) );
-                ViewData[ "listSuggestedAnnounces" ] =
-                   CommonFunctions.GetSuggestedAnnounces( _context,this ).Take( 3 ).ToList();
-            } else {
-                ViewData[ "listSuggestedAnnounces" ] = new List< Announce >();
-            }
-            CommonFunctions.SetRootLayoutViewData( this,_context );
-            ViewData["listCategory"] = _context.Categories.ToList();
-            return View();
-        }
+         if( LoginChecker.HasLoggedUser( this ) ) {
+            var user = _context.Users.Single( u => User.GetUserId().Equals( u.Id ) );
+            ViewData[ "listSuggestedAnnounces" ] = GetSuggestedAnnounces( _context, this ).Take( 3 ).ToList();
+         } else
+            ViewData[ "listSuggestedAnnounces" ] = new List< Announce >();
+         SetRootLayoutViewData( this, _context );
+         ViewData[ "listCategory" ] = ViewData[ "formCategories" ];
+         return View();
+      }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+      public IActionResult About() {
+         ViewData[ "Message" ] = "Your application description page.";
 
-            return View();
-        }
+         return View();
+      }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+      public IActionResult Contact() {
+         ViewData[ "Message" ] = "Your contact page.";
 
-            return View();
-        }
+         return View();
+      }
 
-        public IActionResult Error() { return View(); }
-    }
+      public IActionResult Error() { return View(); }
+   }
 }
