a4d76466cacb59672a2008510d311fea5b37795a
raffa50 <reloader90@gmail.com>
2016-05-31 08:03:08 +0200

    Refactoring HistoryController rimossa una ')'

--- Cianfrusaglie/src/Cianfrusaglie/Controllers/HistoryController.cs
+++ Cianfrusaglie/src/Cianfrusaglie/Controllers/HistoryController.cs
@@ -27,24 +27,6 @@ namespace Cianfrusaglie.Controllers {
       } 
 
       /// <summary>
-      /// Ritorna una dizionario contenente gli utenti interessati ad un mio annuncio.
-      /// </summary>
-      /// <returns>Dizionario la cui chiave è l'id dell'annuncio e il valore è la lista degli id degli utenti interessati</returns>
-      public Dictionary<int,List<int>> GetInterestedToAnnounces(){
-            var announces = GetLoggedUserPublishedAnnounces();
-            var dictionary = new Dictionary<int, List<int>>();
-            foreach(var announce in announces)
-            {
-                var interessati = new List<int>();
-                interessati.Add(_context.Interested.Where(i => i.AnnounceId.Equals(announce.Id)).ToList().Count);
-                interessati.Add(_context.Interested.Where(i => i.AnnounceId.Equals(announce.Id) && !i.Read).ToList().Count);
-                dictionary.Add(announce.Id, interessati);
-            }
-            return dictionary;
-        
-      }
-
-      /// <summary>
       /// Questo metodo carica la pagina con tutti gli annunci pubblicati dall'utente loggato (membro)
       /// </summary>
       /// <returns>La View di tutti i tuoi annunci</returns>
@@ -53,7 +35,7 @@ namespace Cianfrusaglie.Controllers {
          if( !LoginChecker.HasLoggedUser( this ) )
             return HttpBadRequest();
         CommonFunctions.SetRootLayoutViewData( this, _context );
-        ViewData["Interested"] = GetInterestedToAnnounces();
+
         return View( GetLoggedUserPublishedAnnounces().ToList() );
       }
       
