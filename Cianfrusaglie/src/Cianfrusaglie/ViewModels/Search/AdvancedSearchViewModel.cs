f2e965f5d755fd64811ad81c0b1fc68a4b5aa41f
Luca Demetrio <uncd3m3@gmail.com>
2016-06-03 12:16:37 +0200

    Creato il ViewModel per la ricerca avanzata.

---
+++ Cianfrusaglie/src/Cianfrusaglie/ViewModels/Search/AdvancedSearchViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cianfrusaglie.ViewModels.Search
+{
+    public class AdvancedSearchViewModel
+    {
+        public string Title { get; set; }
+
+        public bool? OrderByDate { get; set; }
+
+        public Tuple<int, int> KmRange { get; set; }
+
+        public Tuple<int, int> PriceRange { get; set; }
+
+        public Tuple<int, int> FeedbackRatingRange { get; set; }
+    }
+}
