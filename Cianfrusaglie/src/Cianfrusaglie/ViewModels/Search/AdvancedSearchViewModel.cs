0cd1df4fbc817f5851e13821e11d0f49e29c1205
Elena <Elena@ELENA>
2016-06-06 17:02:05 +0200

    interfaccia ricerca avanzata (ancora da collegare alla logica)

--- Cianfrusaglie/src/Cianfrusaglie/ViewModels/Search/AdvancedSearchViewModel.cs
+++ Cianfrusaglie/src/Cianfrusaglie/ViewModels/Search/AdvancedSearchViewModel.cs
@@ -9,7 +9,9 @@ namespace Cianfrusaglie.ViewModels.Search
     {
         public string Title { get; set; }
 
-        public bool OrderByDate { get; set; }
+        public bool? OrderByDateAscending { get; set; }
+
+        public bool? OrderByDateDescending { get; set; }
 
         public Tuple<int, int> KmRange { get; set; }
 
