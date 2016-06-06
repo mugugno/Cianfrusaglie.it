b7ddeb6268183c146d901d3ea0be7719f9bb3887
Luca Demetrio <uncd3m3@gmail.com>
2016-06-06 16:29:36 +0200

    La logica della ricerca avanzata non va ancora bene. Li devo finire, idem per i test!

--- Cianfrusaglie/test/Cianfrusaglie.Tests/SearchControllerTests.cs
+++ Cianfrusaglie/test/Cianfrusaglie.Tests/SearchControllerTests.cs
@@ -7,6 +7,7 @@ using Cianfrusaglie.ViewModels.Search;
 using Microsoft.AspNet.Mvc;
 using Moq;
 using Xunit;
+using Microsoft.Data.Entity;
 
 namespace Cianfrusaglie.Tests {
     public class SearchControllerTests : BaseTestSetup {
@@ -145,7 +146,7 @@ namespace Cianfrusaglie.Tests {
         {
             var user = Context.Users.Single(u => u.UserName.Equals(FirstUserName));
             var searchController = CreateResearchController(user.Id);
-            AdvancedSearchViewModel advSearchByTitle = new AdvancedSearchViewModel() {Title = "Halo"};
+            var advSearchByTitle = new AdvancedSearchViewModel() {Title = "Halo"};
             var announce =
                 Context.Announces.Single(ann => ann.Author.UserName == FirstUserName && ann.Title == "Halo 5 Usato");
             var result = searchController.PerformAdvancedSearch(advSearchByTitle);
@@ -157,7 +158,7 @@ namespace Cianfrusaglie.Tests {
         {
             var user = Context.Users.Single(u => u.UserName.Equals(FirstUserName));
             var searchController = CreateResearchController(user.Id);
-            AdvancedSearchViewModel advSearchByPriceRange = new AdvancedSearchViewModel()
+            var advSearchByPriceRange = new AdvancedSearchViewModel()
             {
                 PriceRange = new Tuple<int, int>(500, 1500)
             };
@@ -179,7 +180,7 @@ namespace Cianfrusaglie.Tests {
             user.Longitude = 0.0;
             Context.SaveChanges();
             var searchController = CreateResearchController(user.Id);
-            AdvancedSearchViewModel advSearchByKmRange = new AdvancedSearchViewModel()
+            var advSearchByKmRange = new AdvancedSearchViewModel()
             {
                 KmRange = new Tuple<int, int>(0, 10)
             };
@@ -201,13 +202,123 @@ namespace Cianfrusaglie.Tests {
             user2.FeedbacksMean = 2.0;
             Context.SaveChanges();
             var searchController = CreateResearchController(user.Id);
-            AdvancedSearchViewModel advSearchByFeedback = new AdvancedSearchViewModel() {FeedbackRatingRange = new Tuple<int, int>(3,5)};
+            var advSearchByFeedback = new AdvancedSearchViewModel() {FeedbackRatingRange = new Tuple<int, int>(3,5)};
             var announce = Context.Announces.First(ann => ann.AuthorId.Equals(user.Id));
             var result = searchController.PerformAdvancedSearch(advSearchByFeedback);
             var wrongAnnounce = Context.Announces.First(ann => ann.AuthorId.Equals(user2.Id));
 
             Assert.Contains(announce, result);
             Assert.DoesNotContain(wrongAnnounce, result);
+        }
+
+        [Fact]
+        public void UserRequestSearchResultOrderedByDateAndIsOk() {
+            var user = Context.Users.Single(u => u.UserName.Equals(FirstUserName));
+            var searchController = CreateResearchController(user.Id);
+            user.Latitude = 0.0;
+            user.Longitude = 0.0;
+            var advOrderDate = new AdvancedSearchViewModel()
+            {
+                OrderByDate = true,
+                ShowGifts = true,
+                ShowOnSale = true
+            };
+            var result = searchController.PerformAdvancedSearch(advOrderDate);
+            var announces = Context.Announces
+                .OrderBy( a=>a.PublishDate ).ToList();
+            Assert.Equal( announces, result );
+        }
+
+        [Fact]
+        public void UserRequestSearchResultNotOrderedByDateAndIsOk()
+        {
+            var user = Context.Users.Single(u => u.UserName.Equals(FirstUserName));
+            var searchController = CreateResearchController(user.Id);
+            user.Latitude = 0.0;
+            user.Longitude = 0.0;
+            var advOrderDate = new AdvancedSearchViewModel()
+            {
+                OrderByDate = false,
+                ShowGifts = true,
+                ShowOnSale = true
+            };
+            
+            var result = searchController.PerformAdvancedSearch(advOrderDate).Select(a=>a.Id);
+            var announces = Context.Announces.OrderBy(a=>a.PublishDate).Select(a => a.Id).ToList();
+            Assert.NotEqual(announces, result);
+        }
+
+        [Fact]
+        public void UserRequestSearchResultOrderdByPriceAscendingAndIsOk() {
+            var user = Context.Users.Single(u => u.UserName.Equals(FirstUserName));
+            var searchController = CreateResearchController(user.Id);
+            var advOrderDate = new AdvancedSearchViewModel()
+            {
+                OrderByPrice = true,
+                ShowGifts = true,
+                ShowOnSale = true
+            };
+            var result = searchController.PerformAdvancedSearch(advOrderDate).Select(a => a.Id).ToList();
+            var announces = Context.Announces
+                .OrderBy(a => a.Price).Select(a => a.Id).ToList();
+            Assert.Equal(announces, result);
+        }
+
+        [Fact]
+        public void UserRequestSearchResultOrderdDescendingByPriceAndIsOk()
+        {
+            var user = Context.Users.Single(u => u.UserName.Equals(FirstUserName));
+            var searchController = CreateResearchController(user.Id);
+            var advOrderDate = new AdvancedSearchViewModel()
+            {
+                OrderByPrice = false,
+                ShowGifts = true,
+                ShowOnSale = true
+            };
+            var result = searchController.PerformAdvancedSearch(advOrderDate).Select(a=>a.Id).ToList();
+            var announces = Context.Announces
+                .OrderByDescending(a => a.Price).Select(a => a.Id).ToList();
+            Assert.Equal(announces, result);
+
+        }
+
+        [Fact]
+        public void UserRequestSearchResultOrderdByRangeAscendingAndIsOk()
+        {
+
+        }
+
+        [Fact]
+        public void UserRequestSearchResultOrderdDescendingByDistanceAndIsOk()
+        {
+
+        }
+
+        [Fact]
+        public void UserRequestOnlyGiftAndIsOk() {
+            var user = Context.Users.Single(u => u.UserName.Equals(FirstUserName));
+            var searchController = CreateResearchController(user.Id);
+            var adv = new AdvancedSearchViewModel()
+            {
+                ShowGifts = true
+            };
+            var result = searchController.PerformAdvancedSearch(adv).Select(a => a.Id).ToList();
+            var announces = Context.Announces.Where( a => a.Price == 0 ).Select(a=>a.Id).ToList();
+            Assert.Equal(announces, result);
+        }
+
+        [Fact]
+        public void UserRequestOnlySalesAndIsOk()
+        {
+            var user = Context.Users.Single(u => u.UserName.Equals(FirstUserName));
+            var searchController = CreateResearchController(user.Id);
+            var adv = new AdvancedSearchViewModel()
+            {
+                ShowOnSale = true
+            };
+            var result = searchController.PerformAdvancedSearch(adv).Select(a => a.Id).ToList();
+            var announces = Context.Announces.Where(a => a.Price != 0).Select(a => a.Id).ToList();
+            Assert.Equal(announces, result);
         }
     }
 
