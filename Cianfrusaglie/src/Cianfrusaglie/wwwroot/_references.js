423f6b53d02546eb94e75139a80e5c5e8f59d69e
raffa50 <reloader90@gmail.com>
2016-06-08 12:35:54 +0200

    password meter (da migliorare)

--- Cianfrusaglie/src/Cianfrusaglie/wwwroot/_references.js
+++ Cianfrusaglie/src/Cianfrusaglie/wwwroot/_references.js
@@ -8,6 +8,7 @@
 /// <reference path="js/googlemaps.js" />
 /// <reference path="js/jquery.menu-aim.js" />
 /// <reference path="js/ladda.js" />
+/// <reference path="js/passwordmeter.js" />
 /// <reference path="js/prism.js" />
 /// <reference path="js/site.js" />
 /// <reference path="js/spin.js" />
