5302af2d18c1c7211487dd0bd7fbfd993b8221c4
raffa50 <reloader90@gmail.com>
2016-06-09 20:07:37 +0200

    tasto share fb migliorato (da finire)

--- Cianfrusaglie/src/Cianfrusaglie/wwwroot/js/site.js
+++ Cianfrusaglie/src/Cianfrusaglie/wwwroot/js/site.js
@@ -11,3 +11,11 @@ function displayTextMaxLen(maxLen, input, feedback) {
         feedback.html(text_remaining + "/"+ maxLen);
     });
 }
+
+function openFbPopUp(fburl, fbimgurl, fbtitle, fbsummary) {
+    var sharerURL= "http://www.facebook.com/sharer/sharer.php?s=100&p[url]=" + encodeURI(fburl) + "&p[images][0]=" + encodeURI(fbimgurl) + "&p[title]=" + encodeURI(fbtitle) + "&p[summary]=" + encodeURI(fbsummary);
+    window.open(
+      sharerURL,
+      "facebook-share-dialog",
+      "width=626,height=436");
+}
